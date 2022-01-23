3caf5f5e6059db252eac3ec2fa896a57fc75fc17
madison-craig <madisoncraig@protonmail.com>
2022-01-22 19:44:21 -0500

    changed file paths

--- arch-install.sh
+++ arch-install.sh
@@ -17,11 +17,11 @@ function CYAN(){
 }
 
 
-exit 1
-
-CYAN "Updating the system!.."
+CYAN "Madison's Arch (with dwm+polybar) install script, Hope you enjoy!"
+sleep 2
+CYAN "Updating the system.."
 sudo pacman --noconfirm -Syu
-
+exit 1
 CYAN "Installing base system & needed packages for base install.."
 sudo pacman --noconfirm -S base-devel git kitty wget xdg-utils xorg xorg-server xorg-xinit yajl zsh
 
@@ -44,71 +44,70 @@ sudo pacman --noconfirm -S noto-fonts noto-fonts-extra noto-fonts-cjk gsfonts ne
 CYAN "Installing packages for the rice.."
 sudo pacman --noconfirm -S dunst feh firefox jsoncpp lxappearance neofetch neovim nitrogen nodejs okular pavucontrol pulseaudio playerctl python-yaml ranger w3m zathura zathura-pdf-poppler
 
-CYAN "Installing dotfiles.."
-git clone https://github.com/madison-craig/dotfiles.git
+CYAN "Installing dotfiles"
 mkdir -p ~/.config/
   if [ -d ~/.config/dunst ]; then
     CYAN "Dunst config already installed, backing up & installing new dunst config.."
     mkdir ~/.config/dunst.old && mv ~/.config/dunst/* ~/.config/dunst.old/
-    cp -r ./dotfiles/dunst/.config/dunst/* ~/.config/dunst/;
+    cp -r ./dunst/.config/dunst/* ~/.config/dunst/;
   else
     CYAN "Installing dusnt config.."
-    mkdir ~/.config/dunst && cp -r ./dotfiles/dunst/.config/dunst/* ~/.config/dunst;
+    mkdir ~/.config/dunst && cp -r ./dunst/.config/dunst/* ~/.config/dunst;
   fi
   if [ -d ~/.config/kitty ]; then
     CYAN "Kitty config already installed, backing up & installing new kitty config.."
     mkdir ~/.config/kitty.old && mv ~/.config/kitty/* ~/.config/kitty.old/
-    cp -r ./dotfiles/kitty/.config/kitty/* ~/.config/kitty/;
+    cp -r ./kitty/.config/kitty/* ~/.config/kitty/;
   else
     CYAN "Installing kitty config.."
-    mkdir ~/.config/kitty && cp -r ./dotfiles/kitty/.config/kitty/* ~/.config/kitty;
+    mkdir ~/.config/kitty && cp -r ./kitty/.config/kitty/* ~/.config/kitty;
   fi
   if [ -d ~/.config/neofetch ]; then
     CYAN "Neofetch config already installed, backing up & installing new neofetch config.."
     mkdir ~/.config/neofetch.old && mv ~/.config/neofetch/* ~/.config/neofetch.old/
-    cp -r ./dotfiles/neofetch/.config/neofetch/* ~/.config/neofetch/;
+    cp -r ./neofetch/.config/neofetch/* ~/.config/neofetch/;
   else
     CYAN "Installing neofetch config.."
-    mkdir ~/.config/neofetch && cp -r ./dotfiles/neofetch/.config/neofetch/* ~/.config/neofetch;
+    mkdir ~/.config/neofetch && cp -r ./neofetch/.config/neofetch/* ~/.config/neofetch;
   fi
   if [ -d ~/.config/nvim ]; then
     CYAN "Neovim config already installed, backing up & installing new neovim config.."
     mkdir ~/.config/nvim.old && mv ~/.config/nvim/* ~/.config/nvim.old/
-    cp -r ./dotfiles/nvim/.config/nvim/* ~/.config/nvim/;
+    cp -r ./nvim/.config/nvim/* ~/.config/nvim/;
   else
     CYAN "Installing neovim config.."
-    mkdir ~/.config/nvim/ && cp -r ./dotfiles/nvim/.config/nvim/* ~/.config/nvim/;
+    mkdir ~/.config/nvim/ && cp -r ./nvim/.config/nvim/* ~/.config/nvim/;
   fi
   if [ -d ~/.config/picom ]; then
     CYAN "Picom config already installed, backing up & installing new picom config.."
     mkdir ~/.config/picom.old && mv ~/.config/picom/* ~/.config/picom.old/
-    cp -r ./dotfiles/picom/.config/picom/* ~/.config/picom/;
+    cp -r ./picom/.config/picom/* ~/.config/picom/;
   else 
     CYAN "Installing picom config.."
-    mkdir ~/.config/picom/ && cp -r ./dotfiles/picom/.config/picom/* ~/.config/picom/;
+    mkdir ~/.config/picom/ && cp -r ./picom/.config/picom/* ~/.config/picom/;
   fi
   if [ -d ~/.config/polybar ]; then
     CYAN  "Polybar config already installed, backing up & installing new polybar config.."
     mkdir ~/.config/polybar.old && mv ~/.config/polybar/* ~/.config/polybar.old/
-    cp -r ./dotfiles/polybar/.config/polybar/* ~/.config/polybar/;
+    cp -r ./polybar/.config/polybar/* ~/.config/polybar/;
   else
     CYAN "Installing polybar config.."
-    mkdir ~/.config/polybar/ && cp -r ./dotfiles/polybar/.config/polybar/* ~/.config/polybar/;
+    mkdir ~/.config/polybar/ && cp -r ./polybar/.config/polybar/* ~/.config/polybar/;
   fi
   if [ -d ~/.config/ranger/ ]; then
     CYAN "Ranger config already installed, backing up & installing new ranger config.."
     mkdir ~/.config/ranger.old && mv ~/.config/ranger/* ~/.config/ranger.old/
-    cp -r ./dotfiles/ranger/.config/ranger/* ~/.config/ranger/;
+    cp -r ./ranger/.config/ranger/* ~/.config/ranger/;
   else
     CYAN "Installing ranger config"
-    mkdir ~/.config/ranger/ && cp -r ./dotfiles/polybar/.config/polybar/* ~/.config/polybar/;
+    mkdir ~/.config/ranger/ && cp -r ./polybar/.config/polybar/* ~/.config/polybar/;
   fi
   if [ -d ~/.config/zathura/ ]; then
     CYAN "Zathura config already installed, backing up 7 installing new zathura config.."
     mkdir ~/.config/zathura.old && mv ~/.config/zathura/* ~/.config/zathura.old/
-    cp -r ./dotfiles/zathura/.config/zathura/* ~/.config/zathura;
+    cp -r ./zathura/.config/zathura/* ~/.config/zathura;
   else
-    mkdir ~/.config/zathura/ && cp -r ./dotfiles/zathura/.config/zathura/* ~/.config/zathura/;
+    mkdir ~/.config/zathura/ && cp -r ./zathura/.config/zathura/* ~/.config/zathura/;
   fi
   if [ -d ~/Pictures/Wallpapers ]; then
     CYAN "Adding wallpaper to ~/Pictures/wallpapers"
@@ -117,5 +116,3 @@ mkdir -p ~/.config/
     CYAN "Installing wallpaper"
     cp ./wallpapers/024.png ~/Pictures/wallpapers/;
   fi
-
-
