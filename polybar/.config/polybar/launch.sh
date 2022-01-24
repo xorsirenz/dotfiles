8d80aa243e06e6f6fd732d1a131d5b80e270d6fc
madison <69394836+xorsirenz@users.noreply.github.com>
2022-01-24 15:05:18 -0500

    one bar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-BAR_NAME=workspace
+BAR_NAME=main
 BAR_NAME1=time
 BAR_NAME2=settings
 
@@ -10,7 +10,7 @@ for item in $(polybar --list-monitors); do
     MONITOR=$(echo $item | cut -d" " -f1 | cut -d":" -f1)
     IS_PRIMARY=$(echo $item | cut -d" " -f3)
     if [[ $IS_PRIMARY == *"primary"* ]]; then
-        TRAY_POSITION=right MONITOR=$MONITOR polybar --reload $BAR_NAME & polybar --reload $BAR_NAME1 & polybar --reload $BAR_NAME2
+        TRAY_POSITION=right MONITOR=$MONITOR polybar --reload $BAR_NAME
     else
         MONITOR=$MONITOR polybar --reload $BAR_NAME &
     fi
