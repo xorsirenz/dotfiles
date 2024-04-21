5be858649fda3662bd8355d2e04877f3cc8c8bfb
madison <69394836+xorsirenz@users.noreply.github.com>
2024-04-21 09:21:07 -0700

    signcolumn off

--- nvim/.config/nvim/lua/core/set.lua
+++ nvim/.config/nvim/lua/core/set.lua
@@ -8,13 +8,18 @@ vim.opt.linebreak = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.autoindent = true
 
 vim.opt.showmode = false
 vim.opt.scrolloff = 2
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.background = "dark"
+vim.opt.signcolumn = "no"
 
 vim.opt.updatetime = 50
-
