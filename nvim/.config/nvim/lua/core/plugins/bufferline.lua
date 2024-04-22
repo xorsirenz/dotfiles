cccd2752844c0476c96268b931c75a2700738f75
madison <69394836+xorsirenz@users.noreply.github.com>
2024-04-22 14:41:59 -0700

    cleanupp

--- nvim/.config/nvim/lua/core/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/core/plugins/bufferline.lua
@@ -2,7 +2,6 @@ return {
     "akinsho/bufferline.nvim",
     tag = "*",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-
     config = function()
         require('bufferline').setup{
             options = {
