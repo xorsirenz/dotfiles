634c1afb31726481d8647fd9ae8f78388913e431
madison <69394836+xorsirenz@users.noreply.github.com>
2024-08-22 16:22:28 -0700

    keybind

--- nvim/.config/nvim/lua/core/plugins/telescope.lua
+++ nvim/.config/nvim/lua/core/plugins/telescope.lua
@@ -11,7 +11,7 @@ return {
 	    local builtin = require('telescope.builtin')
 	    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 	    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-	    vim.keymap.set('n', '<leader>ps', function()
+	    vim.keymap.set('n', '<leader>pws', function()
 		    local word = vim.fn.expand("<cword>")
 		    builtin.grep_string({ search = word })
 	    end)
