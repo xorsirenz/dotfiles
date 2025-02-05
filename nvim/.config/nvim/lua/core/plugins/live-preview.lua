7104af9a3552d09d2ca8e9bcdbd4ad8c2f7989ff
madison <69394836+xorsirenz@users.noreply.github.com>
2025-02-04 19:54:48 -0700

    added live preview

---
+++ nvim/.config/nvim/lua/core/plugins/live-preview.lua
@@ -0,0 +1,9 @@
+return {
+    'brianhuster/live-preview.nvim',
+    dependencies = {
+        -- You can choose one of the following pickers
+        'nvim-telescope/telescope.nvim',
+        'ibhagwan/fzf-lua',
+        'echasnovski/mini.pick',
+    },
+}
