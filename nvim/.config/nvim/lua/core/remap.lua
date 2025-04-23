fa997dba54942da5899e135b2dce24a5e73099d9
madison <69394836+xorsirenz@users.noreply.github.com>
2025-04-22 17:54:18 -0700

    set jq to leader keybind jq

--- nvim/.config/nvim/lua/core/remap.lua
+++ nvim/.config/nvim/lua/core/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":q<CR>")
-vim.keymap.set("n", "<leader>pj", ":%!jq .<CR>")
+vim.keymap.set("n", "<leader>jq", ":%!jq .<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
