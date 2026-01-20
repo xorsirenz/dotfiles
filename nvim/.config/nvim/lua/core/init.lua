b95deafe0899577b8281af567af3e6852e56800c
madison <69394836+xorsirenz@users.noreply.github.com>
2026-01-19 22:40:33 -0800

    change inlay hints from leader ei/di to leader eh/dh

--- nvim/.config/nvim/lua/core/init.lua
+++ nvim/.config/nvim/lua/core/init.lua
@@ -18,8 +18,8 @@ autocmd('LspAttach', {
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>ei", function() vim.lsp.inlay_hint.enable() end, opts)
-        vim.keymap.set("n", "<leader>di", function() vim.lsp.inlay_hint.enable(false) end, opts)
+        vim.keymap.set("n", "<leader>eh", function() vim.lsp.inlay_hint.enable() end, opts)
+        vim.keymap.set("n", "<leader>dh", function() vim.lsp.inlay_hint.enable(false) end, opts)
 
     end
 })
