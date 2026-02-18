a46f853f8ed333555f2cabff5fe8c8dc775ab08a
madison <69394836+xorsirenz@users.noreply.github.com>
2026-02-18 09:28:43 -0800

    changed keymap for git files to pg

--- nvim/.config/nvim/lua/core/plugins/telescope.lua
+++ nvim/.config/nvim/lua/core/plugins/telescope.lua
@@ -18,7 +18,7 @@ return {
 
 	    local builtin = require('telescope.builtin')
 	    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-	    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+	    vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 	    vim.keymap.set('n', '<leader>pws', function()
 		    local word = vim.fn.expand("<cword>")
 		    builtin.grep_string({ search = word })
