d1e188117e0006641bcfd27b111e3410a9cf2145
madison <69394836+xorsirenz@users.noreply.github.com>
2026-02-18 09:27:00 -0800

    fzf extenstion loaded

--- nvim/.config/nvim/lua/core/plugins/telescope.lua
+++ nvim/.config/nvim/lua/core/plugins/telescope.lua
@@ -7,7 +7,14 @@ return {
     },
 
     config = function()
-	    require('telescope').setup{}
+	    require('telescope').setup{
+
+        extensions = {
+            fzf = {}
+            }
+        }
+
+        require('telescope').load_extension('fzf')
 
 	    local builtin = require('telescope.builtin')
 	    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
