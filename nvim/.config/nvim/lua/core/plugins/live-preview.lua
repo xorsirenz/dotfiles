050d2f1c89f92542d8ea714f811695d12e6ac231
madison <69394836+xorsirenz@users.noreply.github.com>
2026-02-19 20:01:26 -0800

    cleanup

--- nvim/.config/nvim/lua/core/plugins/live-preview.lua
+++ nvim/.config/nvim/lua/core/plugins/live-preview.lua
@@ -1,9 +1,6 @@
 return {
     'brianhuster/live-preview.nvim',
     dependencies = {
-        -- You can choose one of the following pickers
         'nvim-telescope/telescope.nvim',
-        'ibhagwan/fzf-lua',
-        'echasnovski/mini.pick',
     },
 }
