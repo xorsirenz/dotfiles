794717fec8299eb7532f36e7718938c0b31dc34c
madison <69394836+xorsirenz@users.noreply.github.com>
2026-02-20 00:39:42 -0800

    set var

--- nvim/.config/nvim/lua/core/remap.lua
+++ nvim/.config/nvim/lua/core/remap.lua
@@ -1,40 +1,43 @@
+set = vim.keymap.set
+
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", ":q<CR>")
-vim.keymap.set("n", "<leader>jq", ":%!jq .<CR>")
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+set("n", "<leader>pv", ":q<CR>")
+set("n", "<leader>jq", ":%!jq .<CR>")
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+set("v", "J", ":m '>+1<CR>gv=gv")
+set("v", "K", ":m '<-2<CR>gv=gv")
+
+set("n", "J", "mzJ`z")
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+set("n", "n", "nzzzv")
+set("n", "N", "Nzzzv")
 
 
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+set("n", "Q", "<nop>")
+set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+set("n", "<C-k>", "<cmd>cnext<CR>zz")
+set("n", "<C-j>", "<cmd>cprev<CR>zz")
+set("n", "<leader>k", "<cmd>lnext<CR>zz")
+set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
-vim.keymap.set("n", "<leader>ee", "opub fn main() !void {<CR>}<Esc>O")
+-- set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
+set("n", "<leader>ee", "opub fn main() !void {<CR>}<Esc>O")
 
-vim.keymap.set("n", "<leader>r", "<cmd>RangerOpenCurrentDir<CR>")
+set("n", "<leader>r", "<cmd>RangerOpenCurrentDir<CR>")
 
-vim.keymap.set("n", "<leader>lp", "<cmd>LivePreview start<CR>")
+set("n", "<leader>lp", "<cmd>LivePreview start<CR>")
 
-vim.keymap.set("n", "<leader>pw", "<cmd>set wrap!<CR>")
+set("n", "<leader>pw", "<cmd>set wrap!<CR>")
 
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set("n", "<leader><leader>", function()
+set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
