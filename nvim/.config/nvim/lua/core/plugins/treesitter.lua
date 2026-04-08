726cf03a7184d9e98fcddca073c4f25780a110f7
xorsirenz <69394836+xorsirenz@users.noreply.github.com>
2026-04-08 09:34:34 -0700

    ensure installed

--- nvim/.config/nvim/lua/core/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/core/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require('nvim-treesitter.configs').setup {
-		ensure_installed = { "bash", "c", "lua", "python", "rust", "vim", "vimdoc", "query" },
+		ensure_installed = { "bash", "c", "go", "lua", "python", "rust", "vim", "vimdoc", "query", "zig" },
   		sync_install = false,
   		auto_install = true,
   		highlight = {
