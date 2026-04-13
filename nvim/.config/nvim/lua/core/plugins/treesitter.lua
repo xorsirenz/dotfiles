6011a4d1ae79bdcb1f6513ace22c6e80f314fd3c
xorsirenz <69394836+xorsirenz@users.noreply.github.com>
2026-04-13 08:17:48 -0700

    updated for main? i hope

--- nvim/.config/nvim/lua/core/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/core/plugins/treesitter.lua
@@ -1,15 +1,27 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require('nvim-treesitter.configs').setup {
-		ensure_installed = { "bash", "c", "go", "lua", "python", "rust", "vim", "vimdoc", "query", "zig" },
-  		sync_install = false,
-  		auto_install = true,
-  		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-			},
-		}
-	end
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    config = function()
+        require('nvim-treesitter').setup {
+            sync_install = false,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            init = function()
+                local ensureInstalled = {
+                    "bash", "c", "go", "lua", "python", "rust", "vim", "vimdoc", "query", "zig"
+                }
+                local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+                local parsersToInstall = vim.iter(ensureInstalled)
+                    :filter(function(parser)
+                        return not vim.tbl_contains(alreadyInstalled, parser)
+                    end)
+                    :totable()
+                require('nvim-treesitter').install(parsersToInstall)
+            end,
+        }
+    end
 }
